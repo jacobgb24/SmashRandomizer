29ceebeb738c4af861ace8160cda1a01c49b43ee
jacobgb24 <jacobgb24@yahoo.com>
2019-03-16 19:55:42 -0600

    added image resources to assets folder
    Built basic character class and gridview adapter in main activity for proof of concept

--- app/src/main/java/com/jacobgb24/smashrandomizer/MainActivity.kt
+++ app/src/main/java/com/jacobgb24/smashrandomizer/MainActivity.kt
@@ -2,11 +2,18 @@ package com.jacobgb24.smashrandomizer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var adapter: CharacterSelectionAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var characters = generateCharacters(this)
+        adapter = CharacterSelectionAdapter(this, characters)
+
+        grid_character_selection.adapter = adapter
     }
 }
