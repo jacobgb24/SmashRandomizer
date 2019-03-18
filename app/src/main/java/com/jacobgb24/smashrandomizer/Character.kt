477a1220321137270d06634c9e87f67e559aac4f
jacobgb24 <jacobgb24@yahoo.com>
2019-03-17 19:54:46 -0600

    Handled edge cases for character names Improved gridview to recycle views and use glide for image loading kotlin is cool

--- app/src/main/java/com/jacobgb24/smashrandomizer/Character.kt
+++ app/src/main/java/com/jacobgb24/smashrandomizer/Character.kt
@@ -1,30 +1,58 @@
 package com.jacobgb24.smashrandomizer
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.util.Log
 
-data class Character(val name: String, val imageName: String)
+class Character(val name: String, val imageName: String, var isSelected: Boolean = true) {
 
+    fun getIconUri(): Uri {
+        return Uri.parse("file:///android_asset/icons/$imageName")
+
+    }
+
+    fun getPortraiUri(): Uri {
+        return Uri.parse("file:///android_asset/portraits/$imageName")
+    }
+
+    fun toggle() {
+        isSelected = !isSelected
+    }
+
+}
 
 
 fun generateCharacters(context: Context): ArrayList<Character> {
-    var characters = ArrayList<Character>()
+    val characters = ArrayList<Character>()
 
-    var portraits = context.getAssets().list("portraits")
+    val portraits = context.assets.list("portraits")
     for (image in portraits) {
-        var name = ""
-        name += image[0].toUpperCase()
-        for (char in image.substring(1, image.indexOf("."))) {
-            if (char.isLowerCase()) {
-                name += char
-            }
-            else {
-                name += " " + char.toUpperCase()
-            }
-
+        val name = when(image) {
+            // handle special cases, otherwise do default parsing
+            "kingKRool.webp" -> "King K. Rool"
+            "rob.webp" -> "R.O.B."
+            "drMario.webp" ->"Dr. Mario"
+            "bowserJr.webp" -> "Bowser Jr."
+            "mrGame&Watch.webp" -> "Mr. Game & Watch"
+            "pacman.webp" -> "PAC-MAN"
+            "pokemonTrainer.webp" -> "Pokémon Trainer"
+            else -> getNameFromPath(image)
         }
+
         characters.add(Character(name, image))
     }
 
     return characters
+}
+
+fun getNameFromPath(image: String): String {
+    var name = "${image[0].toUpperCase()}"
+    for (char in image.substring(1, image.indexOf("."))) {
+        when {
+            char.isLowerCase() -> name += char
+            else -> name += " $char"
+        }
+    }
+    return name
 }
--- app/src/main/java/com/jacobgb24/smashrandomizer/CharacterSelectionAdapter.kt
+++ app/src/main/java/com/jacobgb24/smashrandomizer/CharacterSelectionAdapter.kt
@@ -2,21 +2,20 @@ package com.jacobgb24.smashrandomizer
 
 import android.content.Context
 import android.graphics.drawable.Drawable
+import android.media.Image
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.GridView
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item_character_selection.view.*
 
-class CharacterSelectionAdapter: BaseAdapter {
-    var characterList = ArrayList<Character>()
-    var context: Context? = null
-
-    constructor(context: Context, list: ArrayList<Character>) {
-        this.context = context
-        this.characterList = list
-    }
+class CharacterSelectionAdapter(private val context: Context): BaseAdapter() {
+    var characterList = generateCharacters(context)
 
     override fun getCount(): Int {
         return characterList.size
@@ -30,15 +29,25 @@ class CharacterSelectionAdapter: BaseAdapter {
         return position.toLong()
     }
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        Log.e("Loading view", characterList[position].name)
-        var inflater = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        var charView = inflater.inflate(R.layout.item_character_selection, null)
-        var image: Drawable = Drawable.createFromStream(
-            context!!.assets.open("icons/" + characterList[position].imageName), null)
-
-
-        charView.image_character_icon.setImageDrawable(image)
-        return charView
+    class CharacterViewHolder(val v: View, var character: String = "") {
+        var image: ImageView = v.image_character_icon
     }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val viewHolder: CharacterViewHolder?
+        if (convertView == null) {
+            val charView = LayoutInflater.from(context).inflate(R.layout.item_character_selection, parent, false)
+            viewHolder = CharacterViewHolder(charView, characterList[position].name)
+            charView.tag = viewHolder
+        }
+        else {
+            viewHolder = convertView.tag as CharacterViewHolder
+//            Log.e("Reusing viewholder", "for: ${characterList[position].name}\nwas: ${viewHolder.character}")
+        }
+        viewHolder.character = characterList[position].name
+        Glide.with(context).load(characterList[position].getIconUri()).into(viewHolder.image)
+        return viewHolder.v
+    }
+
+
 }
