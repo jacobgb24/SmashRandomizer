6722efe54826391e40634ea0a225d12c61fd3269
jacobgb24 <jacobgb24@yahoo.com>
2019-03-19 19:16:08 -0600

    added font. Built base of class for handling character list. Built some of home screen and got randomization working

--- app/src/main/java/com/jacobgb24/smashrandomizer/MainActivity.kt
+++ app/src/main/java/com/jacobgb24/smashrandomizer/MainActivity.kt
@@ -2,18 +2,31 @@ package com.jacobgb24.smashrandomizer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: CharacterSelectionAdapter
+    var currentCharacter: Character? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainCharacterList = generateCharacters(this)
 
-        adapter = CharacterSelectionAdapter(this)
+        setRandomChar()
 
-        grid_character_selection.adapter = adapter
+        button_random.setOnClickListener {
+            setRandomChar()
+        }
+    }
+
+
+    private fun setRandomChar() {
+        currentCharacter = mainCharacterList.getNewRandom(currentCharacter)
+        Glide.with(this).load(currentCharacter!!.portraitUri).into(image_character_random)
+        text_character_name_random.text = currentCharacter!!.name
+        image_character_random.contentDescription = "Current Character: ${currentCharacter!!.name}"
     }
 }
---
+++ app/src/main/java/com/jacobgb24/smashrandomizer/PoolTools.kt
@@ -0,0 +1,59 @@
+package com.jacobgb24.smashrandomizer
+
+import android.content.Context
+import android.util.Log
+import java.util.*
+import kotlin.collections.ArrayList
+
+// Must be initialized before using any functions. Do so via `mainCharacterList = generateCharacters(context)`
+lateinit var mainCharacterList: ArrayList<Character>
+
+
+fun ArrayList<Character>.getSelected(): ArrayList<Character> = ArrayList(filter { character -> character.isSelected })
+
+fun ArrayList<Character>.getNewRandom(current: Character?): Character {
+    var nextChar: Character
+    do {
+        nextChar = getSelected()[Random().nextInt(size)]
+        Log.e("next vs current", "${nextChar.name} - ${current?.name}")
+    } while(current?.equals(nextChar) == true) // have to do == true to make current being null = false
+    return nextChar
+
+}
+
+
+fun generateCharacters(context: Context): ArrayList<Character> {
+
+    fun getNameFromPath(image: String): String {
+        var name = "${image[0].toUpperCase()}"
+        for (char in image.substring(1, image.indexOf("."))) {
+            name += when {
+                char.isLowerCase() -> char
+                else -> " $char"
+            }
+        }
+        return name
+    }
+
+    val characters = ArrayList<Character>()
+
+    val portraits = context.assets.list("portraits")
+    for (image in portraits) {
+        val name = when(image) {
+            // handle special cases, otherwise do default parsing
+            "kingKRool.webp" -> "King K. Rool"
+            "rob.webp" -> "R.O.B."
+            "drMario.webp" ->"Dr. Mario"
+            "bowserJr.webp" -> "Bowser Jr."
+            "mrGame&Watch.webp" -> "Mr. Game & Watch"
+            "pacman.webp" -> "PAC-MAN"
+            "pokemonTrainer.webp" -> "Pokémon Trainer"
+            else -> getNameFromPath(image)
+        }
+
+        characters.add(Character(name, image, context))
+    }
+
+    return characters
+}
+
