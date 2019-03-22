ddc2cdcdba477c94c12e65493395f7ab9c9bebec
jacobgb24 <jacobgb24@yahoo.com>
2019-03-21 18:04:50 -0600

    added missing files

---
+++ app/src/main/java/com/jacobgb24/smashrandomizer/view/PreferenceFragment.kt
@@ -0,0 +1,38 @@
+package com.jacobgb24.smashrandomizer.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.preference.PreferenceFragmentCompat
+import android.view.MenuItem
+import android.widget.Toast
+import com.jacobgb24.smashrandomizer.R
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.view.View
+
+
+class PreferenceFragment: PreferenceFragmentCompat() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        view!!.setBackgroundColor(resources.getColor(android.R.color.background_light ))
+
+
+        return view
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, null)
+
+        val repeatPref = findPreference("pref_allow_repeat")
+        repeatPref.setOnPreferenceClickListener {
+            Toast.makeText(activity, "This does nothing right now", Toast.LENGTH_SHORT).show()
+            true
+        }
+    }
+
+
+}
+
+
---
+++ app/src/main/java/com/jacobgb24/smashrandomizer/view/RandomFragment.kt
@@ -0,0 +1,83 @@
+package com.jacobgb24.smashrandomizer.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.*
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.jacobgb24.smashrandomizer.*
+import com.jacobgb24.smashrandomizer.controller.MainActivity
+import com.jacobgb24.smashrandomizer.controller.addFragment
+import com.jacobgb24.smashrandomizer.controller.replaceFragment
+import com.jacobgb24.smashrandomizer.model.Character
+import com.jacobgb24.smashrandomizer.model.getNewRandom
+import com.jacobgb24.smashrandomizer.model.mainCharacterList
+import kotlinx.android.synthetic.main.fragment_random.*
+import kotlinx.android.synthetic.main.fragment_random.view.*
+
+
+class RandomFragment : Fragment() {
+
+    var currentCharacter: Character? = null
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    // Create view hierarchy controlled by fragment.
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_random, container, false)
+        Log.e("RandomFrag", "onCreateView")
+
+        view.button_random.setOnClickListener {
+            setRandomChar()
+        }
+
+        view.button_view_pool.setOnClickListener {
+            (activity as MainActivity).replaceFragment(CharacterSelectionFragment())
+        }
+
+        return view
+
+    }
+
+    override fun onStart() {
+        setRandomChar()
+        super.onStart()
+    }
+
+
+
+    private fun setRandomChar() {
+        currentCharacter = mainCharacterList.getNewRandom(currentCharacter)
+        Glide.with(this).load(currentCharacter!!.portraitUri)
+            .transition(DrawableTransitionOptions.withCrossFade()).into(image_character_random)
+        text_character_name_random.text = currentCharacter!!.getOrderString().append(" - ${currentCharacter!!.name}")
+        image_character_random.contentDescription = "Current Character: ${currentCharacter!!.name}"
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater!!.inflate(R.menu.main_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.menu_open_settings -> {
+                (activity as MainActivity).replaceFragment(PreferenceFragment())
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+}
